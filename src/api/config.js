6de4f5f21635d9777871f76161dfd348277f5a73
Gonzalo <pelaogon@gmail.com>
2017-04-10 23:26:06 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,15 @@
+export default {
+	appName: 'platzimusic',
+	apiKey: '85a74d17c0219cc2acc6c2b34399c971',
+  secret: '60deb334cfc887617bcdb2172ae646e2',
+  registeredTo: 'pelaogon'
+}
+
+
+
+
+
+// Application name	  platzimusic
+// API key	           85a74d17c0219cc2acc6c2b34399c971
+// Shared secret	     60deb334cfc887617bcdb2172ae646e2
+// Registered to	     pelaogon
